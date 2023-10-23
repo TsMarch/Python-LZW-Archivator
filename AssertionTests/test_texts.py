4ac4ca5856ffb8b1df194726fc88dab6c44379a0
TsMarch <dementedcactus@mail.ru>
2023-10-23 16:35:06 +0500

    update tests
    Function checking tests created.

---
+++ AssertionTests/test_texts.py
@@ -0,0 +1,17 @@
+
+
+text_1 = """We can't imagine our life without books. They play a very important part in our life. Books are our friends. We meet them when we are very small and can't read, but we remember our mother read them for us. We learn very much from books.
+         Books educate people in different spheres of life. They develop our imagination, make us think and analyse. They help to form our character and the world outlook. Books help us in self education and in deciding problems of life. They make our life more interesting.
+         People read both for knowledge and for pleasure. Different people read different books. They help us with our lessons and work. We read serious books which help us understand the life, give us answers to questions which worry us, they make us think.
+         Many people enjoy so-called "easy reading" - detectives, amusing, humorous stories, fantastic. But so many people, so many tastes.
+         As for me, I prefer to read adventure stories, full of interesting real fact and pictures, and detectives. My favourite author is Arthur Conan Doyle with his Holmes' adventures."""
+
+text_2 = """The United Kingdom of Great Britain and Northern Ireland with an area of 244 000 square miles is situated on the British Isles, which are separated from the European continent by the North Sea, the Strait of Dover and the English Channel. Britain's population is over 56 million. Four out of five people live in towns. The largest cities of the country are London, Birmingham, Liverpool, Manchester, Glasgow and Edinburgh.
+            The UK is a monarchy: the head of the state is a king or a queen. In practice, the Sovereign reigns, but does not rule: the UK is governed by the Government. The present Sovereign is Queen Elizabeth II.
+            The territory of Great Britain is small. Yet the country has a wide variety of scenery.
+            The heart of Britain is England. It is the richest, the most fertile and most populated in the country. The north and the west of England are mountainous, but all the rest of territory is a vast plain. In Northwest England, there are many beautiful lakes with green, wooded or grassy shores and grey mountains all around.
+            The smallest of Britain's countries is Wales. The largest part of Wales is covered with rocky mountains, which are difficult to climb. Most people in Wales live in the coastal plains.
+            Scotland is a land of mountains, wild moorlands, narrow valleys and plains, famous lakes and no end of large and small islands. The Highlands of Scotland are among the oldest mountains in the world. One-third of the people in Scotlandlive in or near its capital, Edinburgh, and its great industrial center, Glasgow.
+            The capital of Great Britain, London, stands on the Thames. The Thames is the busiest and the most important river in Great Britain, but it is not very long. The climate of Great Britain is mild."""
+text_3 = "1"
+text_4 = "漢字"
---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+# Version 1.0.0
+Added an algorithm for archiving and unarchiving LZW. Added text compression calculation function. 
