059772f64f03b7599469aef3b3bea2826a039f6b
Alexey Zhuravlev <81296855+TsMarch@users.noreply.github.com>
2023-10-24 12:21:26 +0500

    Update
    slightly updated comment

--- test_s.py
+++ test_s.py
@@ -3,7 +3,7 @@ from LZW_algorithm.Archivator import *
 from LZW_algorithm.test_texts import *
 
 # Используем фикстуру параметризации pytest для запуска множества тестов.  
-# Тест будет считаться успешно пройденным в том случае, если сжатая строка будет равна строке после распаковки.
+# Тест будет считаться успешно пройденным в том случае, если изначальная строка будет равна строке после распаковки.
 
 @pytest.mark.parametrize('text', [text_1, text_2, text_3, text_4, text_5])
 def test_encoder_decoder(text):
